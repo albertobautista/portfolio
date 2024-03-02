45b35710b35c716a39903d5851d20d13bb79d20f
Alberto Bautista <dachb101@gmail.com>
2024-03-01 20:07:19 -0600

    Move components to section folder

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -10,5 +12,10 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    animations,
+    function ({ addVariant }) {
+      addVariant("any-hover", "@media (any-hover: hover) { &:hover }");
+    },
+  ],
 };
